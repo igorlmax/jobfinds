89a634d3c3c1b3c2643c10dc700096123a7a48a4
Igor Litajkovski <igorlmax@yahoo.com>
2015-12-03 15:17:21 +0100

    Creates Jobs Controller and index.ctp View

--- app/Controller/JobsController.php
+++ app/Controller/JobsController.php
@@ -7,8 +7,14 @@
 		 */
 		public function index(){
 			
+			//$options act as WHERE
+			$options = array(
+				'order' => array('Job.created' => 'asc'),
+				'limit' => '2'
+			);
+			
 			// Get Job Info SELECT * from jobs
-			$jobs = $this->Job->find('all');
+			$jobs = $this->Job->find('all', $options);
 			
 			$this->set('jobs', $jobs);
 		}
--- app/View/Jobs/index.ctp
+++ app/View/Jobs/index.ctp
@@ -1,3 +1,4 @@
+<!-- foreach act as a "for loop" -->
 <?php foreach ($jobs as $job) : ?>
 	<p><?php echo $job['Job']['title']?></p>
 <?php endforeach; ?>
